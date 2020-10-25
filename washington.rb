7f14d161c8d02303661cc12a1a8e3e47f283a514
mbeyushirley <mbeyushirley@gmail.com>
2020-10-25 12:51:21 +0300

    A pinch of Ruby

---
+++ washington.rb
@@ -0,0 +1,18 @@
+#teaching Wash Ruby
+print "Ruby setup was stressful,"
+puts "Wash is helping"
+#After writing code, save first then run
+
+puts x = 10+4
+x=5
+y=3
+puts x+y, x-y, x*y, x/y
+puts x%y, x**y
+puts x+=4, x-=4, x*=4, x/=4
+puts x%=4, x**=4
+puts a,b,c = 10, 20, 30
+ puts s = a+b
+
+
+
+
